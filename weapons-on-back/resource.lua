686fe6b29d7a8666c7653b4f9a2b996a4ec49899
leightonjnr <91348401+leightonjnr@users.noreply.github.com>
2022-03-19 08:44:06 +0000

    Add files via upload

---
+++ weapons-on-back/resource.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'L.Junior'
+description 'places large weapons on the players back'
+version '1.0.0'
+
+client_script 'client.lua'
+
+server_script 'server.lua'
